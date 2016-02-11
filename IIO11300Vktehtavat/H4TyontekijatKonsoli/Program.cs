deff5ed8f36ca7b3cd848d86785bb0cb4bc07bfa
veenus82 <markus.malm@gmail.com>
2016-02-11 11:10:45 +0200

    add demos

---
+++ IIO11300Vktehtavat/H4TyontekijatKonsoli/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace H4TyontekijatKonsoli
+{
+    class Program
+    {
+        static void CalculateSalarySumFromXML(string filu)
+        {
+            try
+            {
+                // tuutkitan onko tiedostoa olemassa
+                if (System.IO.File.Exists(filu))
+                {
+                    // lueataan XML tiedosto XmlDocument-olioon
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(filu);
+                    // haetaan kaikki vakituisten tyontekijöiden palkka-elementit XPathilla
+                    XmlNodeList xnl = xmldoc.SelectNodes("/tyontekijat/tyontekija[tyosuhde='vakituinen']/palkka");
+                    // loopitetaan nodelist läpi
+                    int yht = 0;
+                    for (int i = 0; i < xnl.Count; i++)
+                    {
+                        yht += Convert.ToInt32(xnl.Item(i).InnerText);
+                    }
+                    Console.WriteLine(string.Format("Vakituisia on {0} ja heidän palkat yhtensä {1} ", xnl.Count, yht));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        static void ReadWorkersFromXML(string filu)
+        {
+            try
+            {
+                // tuutkitan onko tiedostoa olemassa
+                if (System.IO.File.Exists(filu))
+                {
+                    // lueataan XML tiedosto XmlDocument-olioon
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(filu);
+                    // haetaan kaikki tyontekija-elementit XPathilla
+                    XmlNodeList xnl = xmldoc.SelectNodes("/tyontekijat/tyontekija");
+                    XmlNode xn; // tämä edustaa yksittäistä nodea
+                    // loopitetaan nodelist läpi
+                    XmlNodeList xnl2;
+                    for (int i = 0; i < xnl.Count; i++)
+                    {
+                        // näytetään käyttäjälle nodejen sisältö
+                        xn = xnl.Item(i);
+                        Console.WriteLine(xn.InnerText);
+                        xnl2 = xn.ChildNodes;
+                        for (int j = 0; j < xnl2.Count; j++)
+                        {
+                            Console.WriteLine(xnl2.Item(j).InnerText);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                //ReadWorkersFromXML("d:\\Työntekijät2013.xml");
+                CalculateSalarySumFromXML("d:\\työntekijät2013.xml");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/H4TyotekijatWPF/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+
+namespace H4TyotekijatWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        XElement xe;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnReadXML_Click(object sender, RoutedEventArgs e)
+        {
+            // luetaan XML-tiedostosta työntekijä-elementit ja sidotaan ne DataGridiin
+            try
+            {
+                string filu = "d:\\työntekijät2013.xml"; // tiedostonimi kovakoodattu, välttäkää kovakoodausta!!
+                XElement xe = XElement.Load(filu);
+                dgData.DataContext = xe.Elements("tyontekija");
+                tbMessage.Text = string.Format("Työntekijöitä {0} ja palkat yhteensä {1}", CountWorkers("vakituinen"), CalculateSalarySum("vakituinen"));
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private int CountWorkers(string tyosuhde)
+        {
+            int lkm = 0;
+            // lasketaan LINQ-kyselyllä tietyntyyppiset työntekijät
+            var temp = from ele in xe.Elements()
+                       where ele.Element("tyosuhde").Value == tyosuhde
+                       select ele.Elements("sukunimi");
+            lkm = temp.Count();
+            return lkm;
+        }
+        private decimal CalculateSalarySum(string tyosuhde)
+        {
+            decimal sum = 0;
+            var palkat = from ele
+                         in xe.Elements()
+                         where ele.Element("tyosuhde").Value == tyosuhde
+                         select ele.Element("palkka").Value;
+            foreach (var item in palkat)
+            {
+                System.Diagnostics.Debug.Print(item.ToString());
+                    sum += decimal.Parse(Item);
+            }
+          
+            return sum;
+        }
+    }
+}
